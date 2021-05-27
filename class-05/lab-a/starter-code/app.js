89fd7cfa099ef503f7cce06263a6bee5dcb8dc91
FarahJamal <farahsarese@gmail.com>
2021-05-27 16:02:53 +0300

    add dynamic multiple array Feature to branch f

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -102,10 +102,10 @@ and when you think it is finished, uncomment the call for the testSumArray() fun
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
-let sumArr=0;
-function sumArray(SumArr) { //eslint-disable-line
+let SumArr=0;
+function sumArray(sumArr) { //eslint-disable-line
   var addArray = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
-  var string = sumArr + ' was passed in as an array of numbers, and ' + addArray + ' is their sum.';
+  var string = sumArr +' was passed in as an array of numbers, and ' + addArray + ' is their sum.';
   return [addArray,string];
 }
 
@@ -150,24 +150,40 @@ testMultiplyArray(testArray);
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length 
+as its argument and returns an array whose first element is the product of those numbers,
+ and the second element is a string that EXACTLY follows this example and concatenates a
+  message using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator *
+ in this function. To do multiplication, use your multiply() 
+ 
+ function that you've already created. You're going to have to be 
+ resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when
+ you think it is finished, uncomment the call for the testMultiplyAnyArray()
+  function and see if the test passes.*/
 
 // Write your code here
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+  function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    var total = dynamicArray[0];
+    
+    for( var i = 1 ; i < dynamicArray.length ; i++ ) {
+      total = multiply(total, dynamicArray[i])[0];
+    }
+    
+    var string = 'The numbers ' + dynamicArray + ' have a product of ' + total + '.';
+    return [total,string];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
